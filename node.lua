0e22719f3eeb65d70f692bee04ca1eff5553c885
Matze <mgriessmeier@suse.com>
2018-01-31 23:07:29 +0100

    Fix typo

--- node.lua
+++ node.lua
@@ -5,7 +5,8 @@ local logo = resource.load_image("img/logo.jpg")
 
 function node.render()
     -- font:write(120,320, "foobar", 100,1,1,1,1)
-    logo.draw(0,0,WIDTH,HEIGHT)
+    gl.clear(1,1,1,1)
+    util.draw_correct(logo,0, 0, WIDTH, HEIGHT)
 end
 
 
