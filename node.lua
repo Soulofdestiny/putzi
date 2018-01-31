e289c8f86c888d65254e1bbf486c274cef53bf63
Matze <mgriessmeier@suse.com>
2018-01-31 22:39:00 +0100

    Render logo

--- node.lua
+++ node.lua
@@ -1,9 +1,11 @@
-gl.setup(1024 ,768)
+gl.setup(1920 ,1080)
 
-local font = resource.load_font('vibes.ttf')
+-- local font = resource.load_font('vibes.ttf')
+local logo = resource.load_image("img/logo.jpg")
 
 function node.render()
-    font:write(120,320, "foobar", 100,1,1,1,1)
+    -- font:write(120,320, "foobar", 100,1,1,1,1)
+    logo.draw(0,0,WIDTH,HEIGHT)
 end
 
 
