6cf44576f77f091bc3c2ee1036b5d49bbd5583b6
Matze <mgriessmeier@suse.com>
2018-02-04 00:36:59 +0100

    Fix typo again

--- node.lua
+++ node.lua
@@ -1,7 +1,7 @@
 gl.setup(1920 ,1080)
 
 -- local font = resource.load_font('vibes.ttf')
-local logo = resource.load_image("img/logo.jpeg")
+local logo = resource.load_image("img/logo.jpg")
 
 function node.render()
     -- font:write(120,320, "foobar", 100,1,1,1,1)
