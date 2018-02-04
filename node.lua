5a1f34ec0eb511199c21f4d7c11c358c18ec3a67
Matze <mgriessmeier@suse.com>
2018-02-04 20:31:30 +0100

    replace logo

--- node.lua
+++ node.lua
@@ -1,12 +1,12 @@
 gl.setup(1920 ,1080)
 
 local font = resource.load_font('vibes.ttf')
-local logo = resource.load_image("img/logo.jpg")
+local logo = resource.load_image("logo.jpg")
 
 function node.render()
+    gl.clear(0,0,0,0)
     font:write(120,320, "foobar", 100,1,1,1,1)
-    gl.clear(1,1,1,1)
-    util.draw_correct(logo,0, 0, WIDTH, HEIGHT)
+    util.draw_correct(logo,WIDTH*0.6, HEIGHT*0.6, WIDTH, HEIGHT)
 end
 
 
